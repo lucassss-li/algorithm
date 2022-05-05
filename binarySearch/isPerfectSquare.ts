38558c5a56649f4b76ef80d3136902031f15516c
lucassss-li <lucas8qwer@gmail.com>
2022-05-05 18:00:10 +0800

    二分法/优化/有效的完全平方数

--- binarySearch/isPerfectSquare.ts
+++ binarySearch/isPerfectSquare.ts
@@ -1,18 +1,17 @@
 // 367. 有效的完全平方数
 
 function isPerfectSquare(num: number): boolean {
-    let i = 0
-    let j = num
-    let mid = 0
-    while (i <= j) {
-        mid = i + Math.floor((j - i) / 2)
+    let i = -1
+    let j = num + 1
+    while (j - i > 1) {
+        const mid = i + Math.floor((j - i) / 2)
         const square = mid * mid
         if (square === num) {
             return true
         } else if (square > num) {
-            j = mid - 1
+            j = mid
         } else {
-            i = mid + 1
+            i = mid
         }
     }
     return false
