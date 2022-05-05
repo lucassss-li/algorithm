e4455293ddcea123ede36ea78686d3598f1d177b
lucassss-li <lucas8qwer@gmail.com>
2022-05-05 17:25:58 +0800

    二分法/优化/寻找比目标字母大的最小字母

--- binarySearch/nextGreatestLetter.ts
+++ binarySearch/nextGreatestLetter.ts
@@ -1,18 +1,15 @@
 // 744. 寻找比目标字母大的最小字母
 
 function nextGreatestLetter(letters: string[], target: string): string {
-    let l = 0
-    let r = letters.length - 1
-    let mid = 0
-    while (l <= r) {
-        mid = l + Math.floor((r - l) / 2)
-        if (letters[mid] === target) {
-            l = mid + 1
-        } else if (letters[mid] > target) {
-            r = mid - 1
+    let i = -1
+    let j = letters.length
+    while (j - i > 1) {
+        const mid = i + Math.floor((j - i) / 2)
+        if (letters[mid] > target) {
+            j = mid
         } else {
-            l = mid + 1
+            i = mid
         }
     }
-    return letters[r + 1] || letters[0]
-}
+    return letters[j]||letters[0]
+};
