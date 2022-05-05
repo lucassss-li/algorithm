d3f30747d5c42ed394614c60644b1915f18dd233
lucassss-li <lucas8qwer@gmail.com>
2022-05-05 19:38:55 +0800

    二分法/优化/有序数组中的单一元素

--- binarySearch/singleNonDuplicate.ts
+++ binarySearch/singleNonDuplicate.ts
@@ -2,17 +2,29 @@
 
 //二段性
 function _singleNonDuplicate(nums: number[]): number {
-    let low = 0,
-        high = nums.length - 1
-    while (low < high) {
-        const mid = Math.floor((high - low) / 2) + low
-        if (nums[mid] === nums[mid ^ 1]) {
-            low = mid + 1
+    let i = -1
+    let j = nums.length - 1
+    while (j - i > 2) {
+        const mid = Math.floor((j - i) / 2) + i
+        if (mid & 1) {
+            if (nums[mid] === nums[mid - 1]) {
+                i = mid
+            } else if (nums[mid] === nums[mid + 1]) {
+                j = mid
+            } else { 
+                return nums[mid]
+            }
         } else {
-            high = mid
+            if (nums[mid] === nums[mid + 1]) {
+                i = mid + 1
+            } else if (nums[mid] === nums[mid - 1]) {
+                j = mid - 1
+            } else { 
+                return nums[mid]
+            }
         }
     }
-    return nums[low]
+    return nums[i + 1]
 }
 
 //异或
@@ -23,3 +35,6 @@ function singleNonDuplicate(nums: number[]): number {
     }
     return res
 }
+
+const res = _singleNonDuplicate([3,3,7,7,10,11,11])
+console.log(res);
