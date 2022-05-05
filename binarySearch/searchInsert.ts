a04d40cad71d06d9472db0255e22b6bc500615bf
lucassss-li <lucas8qwer@gmail.com>
2022-05-05 17:38:51 +0800

    二分法/优化/搜索插入位置

--- binarySearch/searchInsert.ts
+++ binarySearch/searchInsert.ts
@@ -3,33 +3,15 @@
 function searchInsert(nums: number[], target: number): number {
     let l = -1
     let r = nums.length
-    let mid = -1
-    while (l < r) {
-        mid = l + Math.floor((r - l) / 2)
+    while (r - l > 1) {
+        const mid = l + Math.floor((r - l) / 2)
         if (nums[mid] === target) {
             return mid
         } else if (nums[mid] > target) {
             r = mid
         } else {
-            l = mid + 1
+            l = mid
         }
     }
     return r
-}
-
-function _searchInsert(nums: number[], target: number): number {
-    let l = -1
-    let r = nums.length - 1
-    let mid = -1
-    while (l <= r) {
-        mid = l + Math.floor((r - l) / 2)
-        if (nums[mid] === target) {
-            return mid
-        } else if (nums[mid] > target) {
-            r = mid - 1
-        } else {
-            l = mid + 1
-        }
-    }
-    return r + 1
 }
