364cb1d09c69e6b8bbeea744ae479757d29b8193
lucassss-li <lucas8qwer@gmail.com>
2022-05-09 08:25:11 +0800

    双指针/两数之和

---
+++ doublePointer/twoSum.ts
@@ -0,0 +1,17 @@
+// 167. 两数之和 II - 输入有序数组
+
+function twoSum(numbers: number[], target: number): number[] {
+    let i = 0
+    let j = numbers.length - 1
+    while (i < j) {
+        const sum = numbers[i] + numbers[j]
+        if (sum === target) {
+            return [i + 1, j + 1]
+        } else if (sum > target) {
+            j--
+        } else {
+            i++
+        }
+    }
+    return [-1, -1]
+}
