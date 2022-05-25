6a578c319d461a883849087427cb4f6a85abcefb
lucassss-li <lucas8qwer@gmail.com>
2022-05-26 07:49:11 +0800

    搜索/DFS/岛屿数量

---
+++ search/numIslands.ts
@@ -0,0 +1,38 @@
+// 200. 岛屿数量
+
+function numIslands(grid: string[][]): number {
+    const m = grid.length
+    const n = grid[0].length
+    let ans = 0
+    const DIRECTIONS = [
+        [1, 0],
+        [0, 1],
+        [-1, 0],
+        [0, -1]
+    ]
+    for (let k = 0; k < m; k++) {
+        for (let a = 0; a < n; a++) {
+            if (grid[k][a] !== '1') continue
+            const stack = [[k, a]]
+            grid[k][a] = '2'
+            while (stack.length) {
+                const [x, y] = stack.pop() as [number, number]
+                for (const direction of DIRECTIONS) {
+                    const [i, j] = [x + direction[0], y + direction[1]]
+                    if (
+                        i >= 0 &&
+                        i < m &&
+                        j >= 0 &&
+                        j < n &&
+                        grid[i][j] === '1'
+                    ) {
+                        grid[i][j] = '2'
+                        stack.push([i, j])
+                    }
+                }
+            }
+            ans++
+        }
+    }
+    return ans
+}
