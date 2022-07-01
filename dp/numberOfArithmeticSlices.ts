e33389a5bd6eb923b3bfb7a967ddf6221088e16c
lucassss-li <lucas8qwer@gmail.com>
2022-07-01 08:49:53 +0800

    搜索/动态规划/等差数列划分

--- dp/numberOfArithmeticSlices.ts
+++ dp/numberOfArithmeticSlices.ts
@@ -1,28 +1,38 @@
 // 413. 等差数列划分
 
+//暴力搜索
 function numberOfArithmeticSlices(nums: number[]): number {
-    if (nums.length < 3) return 0
     const n = nums.length
-    const visited = new Set()
+    if (n < 3) return 0
     let ans = 0
     for (let i = 0; i < n; i++) {
-        let start = i
-        if (visited.has(start)) continue
-        visited.add(start)
-        for (let j = start + 1; j < n; j++) {
-            if (
-                j - 2 < start ||
-                nums[j] - nums[j - 1] === nums[j - 1] - nums[j - 2]
-            ) {
-                if (j - start >= 2) {
+        for (let j = i + 1; i < n; j++) {
+            if (j - i >= 2) {
+                if (nums[j] - nums[j - 1] === nums[j - 1] - nums[j - 2]) {
                     ans++
+                } else {
+                    break
                 }
-            } else {
-                start = j
-                if (visited.has(start)) break
-                visited.add(start)
             }
         }
+    }
+    return ans
+}
+
+//dp
+function _numberOfArithmeticSlices(nums: number[]): number {
+    const n = nums.length
+    if (n < 3) return 0
+    let ans = 0
+    const dp = Array(n)
+        .fill(0)
+        .map(() => [0, 1])
+    dp[0] = [Infinity, 1]
+    for (let i = 1; i < n; i++) {
+        const [d, n] = dp[i - 1]
+        dp[i][0] = nums[i] - nums[i - 1]
+        dp[i][1] = dp[i][0] === d ? n + 1 : 2
+        dp[i][1] >= 3 && (ans += dp[i][1] - 2)
     }
     return ans
 }
